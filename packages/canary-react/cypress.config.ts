cfa2d4a251da4ac8a9543aeaa50acd35501a5484
oakenroot <294000604+oakenroot@users.noreply.github.com>
2026-07-12 17:36:48 +0100

    chore(canary-react): switch Cypress component tests from Electron to Chrome

--- packages/canary-react/cypress.config.ts
+++ packages/canary-react/cypress.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "cypress";
-import { config } from '../react/cypress.config'
+import { config } from "../react/cypress.config";
 
 export default defineConfig({
-  env: {
-    ci: process.env.CYPRESS_CI ?? false
-  },
   component: {
     ...config.component,
     supportFile: "../react/cypress/support/index.ts",
-    retries: {
-       runMode: 3,
-       openMode: 0
-     },
   },
-} as Cypress.ConfigOptions) 
+});
